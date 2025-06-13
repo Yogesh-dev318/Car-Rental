09e74a6760eba4b86b9336d5a38700f9860eba6d
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-06-13 08:37:33 +0530

    added frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        hlo
+        <Button>click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
